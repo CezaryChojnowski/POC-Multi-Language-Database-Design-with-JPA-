fd094d5d588e8d6925de4728057b5fe2f0404681
CezaryChojnowski <cchojnowski@grupakreos.pl>
2023-03-05 07:34:50 +0100

    Init

---
+++ src/main/java/com/cchojnowski/pocmultilanguage/PocMultiLanguageApplication.java
@@ -0,0 +1,13 @@
+package com.cchojnowski.pocmultilanguage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocMultiLanguageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PocMultiLanguageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cchojnowski/pocmultilanguage/PocMultiLanguageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cchojnowski.pocmultilanguage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocMultiLanguageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
